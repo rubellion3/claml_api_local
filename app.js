d87d01bbb2581f8d0b03f30c98c1e57e9fd54e9d
rubellion3 <pannawat.chs@gmail.com>
2019-05-21 00:56:57 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mysql = require("mysql")
+const app = express()
+
+app.listen(3003), (req,res) =>{
+    console.log("USE 3003 local hsot")
+    res.send("ICD10 API")
+}
+
+const conn = mysql.createConnection({
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "data"
+})
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
