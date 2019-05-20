d5308d4bcd6dfe8b53f31aed75b5692e85e93ff1
rubellion3 <pannawat.chs@gmail.com>
2019-05-21 01:10:53 +0700

    fix #1

--- app.js
+++ app.js
@@ -4,26 +4,28 @@ const app = express()
 
 app.listen(3003), (req,res) =>{
     console.log("USE 3003 local hsot")
-    res.send("ICD10 API")
 }
 
 const conn = mysql.createConnection({
-    host = "localhost",
-    user = "root",
-    password = "root",
-    database = "data"
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'data'
 })
 
 conn.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected to mysql!");
 });
 
+app.get('/',(req,res) =>{
+ res.send("This is an icd10 api makes by sharifz")
+})
 
 app.get('/sharp',(req,res) =>{
     conn.query("SELECT * FROM sharp_code", (err,rows,fields) =>{
         console.log("fecth.....")
-        if(err) throw err
+        if (err) throw err;
             res.json(rows)
     })
 })
