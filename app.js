a8e6b3fd2d92aff29ce3cfb183dabad7f40585a4
rubellion3 <pannawat.chs@gmail.com>
2019-05-29 21:00:04 +0700

    add

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const mysql = require("mysql")
 const app = express()
 const PORT = process.env.PORT || 3003;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.listen(PORT), (req,res) =>{
     console.log("USE 3003 local host")
 }
